c3aae098f42475838c3d65ca0cddd45559a26785
hugocortesm <hugo.cortesm@gmail.com>
2013-05-05 19:33:16 -0500

    gittest_hugo_branch modify About.java (hugo_branch)

--- src/main/java/com/pech/tapestry/pages/About.java
+++ src/main/java/com/pech/tapestry/pages/About.java
@@ -6,6 +6,8 @@ public class About {
 
 	private String content;
 
+	private String otherStuff;
+
 	public String getTitlePage() {
 		return titlePage;
 	}
@@ -20,6 +22,14 @@ public class About {
 
 	public void setContent(String content) {
 		this.content = content;
+	}
+
+	public String getOtherStuff() {
+		return otherStuff;
+	}
+
+	public void setOtherStuff(String otherStuff) {
+		this.otherStuff = otherStuff;
 	}
 
 }
